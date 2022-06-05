f57c4ab9bbee569a8a574daa037832b21cc8e0aa
Vladislav83530 <88848593+Vladislav83530@users.noreply.github.com>
2022-06-05 22:14:22 +0300

    Publish commit
    publish

---
+++ UrbanAreasProject/obj/Release/net5.0/PubTmp/Out/wwwroot/script/app.js
@@ -0,0 +1,11 @@
+﻿$(function () {
+    $("[data-collapse]").on("click", function (event) {
+        event.preventDefault();
+
+        var $this = $(this),
+            blockId = $this.data('collapse');
+
+        $this.toggleClass("active");
+    });
+});
+
